11002a7a98ea6b5944976e6f2056ee9351c02d35
Beatriz <beatriz.rodrigues04@outlook.com>
2025-01-27 12:30:36 -0300

    Atualização dados

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
   .setTitle('Blog Pessoal')
   .setDescription('Projeto Blog Pessoal')
-  .setContact("Generation Brasil","http://www.generationbrasil.online","generation@email.com")
+  .setContact("Beatriz Rodrigues","https://github.com/Beatriz-Rodrigues-P/","beatriz.rodrigues04n@outlook.com")
   .setVersion('1.0')
   .addBearerAuth()
   .build();
